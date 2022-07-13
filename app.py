597e5a7d1affae8f71644228c9a83cbafc5dca3f
Minsun <minsunkim167@gmail.com>
2022-07-13 13:39:18 +0200

    로그아웃 완성2

--- app.py
+++ app.py
@@ -15,23 +15,32 @@ SECRET_KEY = 'SPARTA'
 client = MongoClient('mongodb+srv://test:sparta@cluster0.34r2eiy.mongodb.net/?retryWrites=true&w=majority')
 db = client.roomescape
 
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route("/store", methods=["GET"])
+def listing():
+    stores_list = list(db.stores.find({}, {'_id': False}))
+    return jsonify({'stores': stores_list})
+
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-        return render_template('index.html')
+        return render_template('login.html')
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        # return redirect(url_for("sign_in", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-
-@app.route('/login')
-def login():
-    msg = request.args.get("msg")
-    return render_template('login.html', msg=msg)
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
@@ -95,4 +104,4 @@ def show_comment():
     return jsonify({'comments':comment_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=3000, debug=True)
